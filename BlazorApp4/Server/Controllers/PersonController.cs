4ad545a18718c07e7088b3f18c12e29bb7b3b4af
AndrzejFFGrabowski <Andrzej.f.f.Grabowski@gmail.com>
2023-01-11 00:10:22 +0100

    CnotrollerStart

---
+++ BlazorApp4/Server/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using BlazorApp4.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp4.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        public static List<Person> persons = new List<Person>
+        {
+            new Person { Id = 1,name="na"}
+            ,new Person { Id = 2,name="me"}
+        };
+
+
+        public PersonController()
+        {
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Person>>> GetPerson()
+        {
+            //var res = await Http.GetJsonAsync<List<Person>>("/data");
+
+            //(HttpMethod.Post, "/api/Customer",< List<Person>>("data.json");
+            //persons = await Database.ReadTextAsync();
+            return Ok(persons);
+        }
+    }
+}
